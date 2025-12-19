d6656981d348b69b771a3150c250c2373de728b9
Beyza748 <kahramanbeyza2022@gmail.com>
2025-12-19 17:04:38 +0300

    Update and rename showService.js to movieService.js

--- services/movieService.js
+++ services/movieService.js
@@ -1,10 +1,10 @@
 export async function searchMovies(query) {
     try {
         const response = await fetch(`https://api.tvmaze.com/search/shows?q=${query}`);
-        if (!response.ok) throw new Error("Ağ hatası!");
+        if (!response.ok) throw new Error("API hatası oluştu.");
         return await response.json();
     } catch (error) {
-        console.error("API Hatası:", error);
+        console.error("Hata:", error);
         return [];
     }
 }
